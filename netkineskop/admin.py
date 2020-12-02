2917a72c0e5da2939093b05251156406796f5abe
artur <asihdi@gmail.com>
2020-12-02 10:31:24 +0100

    add tag system models

--- netkineskop/admin.py
+++ netkineskop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Channel, ChannelTag
+
+
+
+admin.site.register(Tag)
+admin.site.register(Channel)
+admin.site.register(ChannelTag)
--- netkineskop/models.py
+++ netkineskop/models.py
@@ -5,7 +5,9 @@ from django.db import models
 
 
 class Tag(models.Model):
-    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name='tags', blank=False, on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=32)
     color = models.CharField(
         max_length=7,
@@ -21,6 +23,7 @@ class Tag(models.Model):
 class Channel(models.Model):
     user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
     yt_channel_id = models.CharField(max_length=64)
+    name = models.CharField(max_length=128)
 
     tags = models.ManyToManyField(Tag, related_name='channels', through='ChannelTag')
 
@@ -29,6 +32,6 @@ class Channel(models.Model):
 
 
 class ChannelTag(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.DO_NOTHING)
+    channel = models.ForeignKey(Channel, on_delete=models.DO_NOTHING)
     favorite = models.BooleanField(default=False)
