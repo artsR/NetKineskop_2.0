46b2b7ac86282f40985d0be507ed99329d6c0751
artur <asihdi@gmail.com>
2020-12-04 21:31:21 +0100

    replace __repr__ with __str__, add uniqueness for M2M through model

--- netkineskop/models.py
+++ netkineskop/models.py
@@ -16,22 +16,30 @@ class Tag(models.Model):
         validators=[RegexValidator(r'^#[0-9a-fA-F]{6}$')]
     )
 
-    def __repr__(self):
+    def __str__(self):
         return f'{self.name}'
 
 
 class Channel(models.Model):
-    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name='channels', blank=False, on_delete=models.CASCADE
+    )
     yt_channel_id = models.CharField(max_length=64)
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, blank=True, default='<name placeholder>')
 
     tags = models.ManyToManyField(Tag, related_name='channels', through='ChannelTag')
 
-    def __repr__(self):
+    def __str__(self):
         return f'{self.name}'
 
 
 class ChannelTag(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.DO_NOTHING)
-    channel = models.ForeignKey(Channel, on_delete=models.DO_NOTHING)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
     favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.tag} : {self.channel}'
+
+    class Meta:
+        unique_together = ('tag', 'channel')
